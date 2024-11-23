480994038221bf50ac01e6a276302bac51e15b50
Anastasiya Gurianova <gurianova.nast@gmail.com>
2024-11-23 08:44:22 +0300

    feat: add custom hook useWindowScroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { useWindowScroll } from './hooks';
 import './App.css';
 
 export const App = () => {
-	return <div></div>;
+	const [scroll, scrollTo] = useWindowScroll();
+
+	return (
+		<div className="container">
+			<p className="info">
+				Scroll position x: {scroll.x}, y: {scroll.y}
+			</p>
+			<button className="button" onClick={() => scrollTo({ y: 0 })}>
+				Scroll to top
+			</button>
+			<div className="spacer"></div>
+		</div>
+	);
 };
